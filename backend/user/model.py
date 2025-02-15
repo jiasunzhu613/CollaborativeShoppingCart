8e110bcf25791bf477197d604f0c9c195c96f7c1
mh-anwar <mohammad.anwar2939@gmail.com>
2025-02-15 01:23:06 -0500

    fix: add/remove columns + fix types

--- backend/user/model.py
+++ backend/user/model.py
@@ -1,8 +1,9 @@
 from typing import List, Optional
 from sqlalchemy.types import String, Uuid
-from sqlalchemy import text
+from sqlalchemy import text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from backend import db
+from datetime import datetime
 import uuid
 
 
@@ -23,10 +24,13 @@ class User(db.Model):
     id: Mapped[uuid.UUID] = mapped_column(
         Uuid, primary_key=True, server_default=text("gen_random_uuid()")
     )
-    email: Mapped[str] = mapped_column()  # TODO: set unique?
-    name: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column(unique=True)
+    first_name: Mapped[str] = mapped_column()
+    last_name: Mapped[str] = mapped_column()
+    dob: Mapped[datetime] = mapped_column(DateTime)
     password_hash: Mapped[str] = mapped_column()
     time_stamp_earliest_valid_token: Mapped[int | None] = mapped_column()
+
     carts_created: Mapped[List["Cart"] | None] = (
         relationship()
     )  # TODO: check if this is even right...
